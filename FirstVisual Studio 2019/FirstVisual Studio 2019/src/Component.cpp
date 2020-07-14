d6cedc4ed0f6c6863441913757c0e15168eb3df0
Saima <star15yuji@gmail.com>
2020-07-14 15:32:25 +0900

    update Component.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/Component.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/Component.cpp
@@ -1,1 +1,18 @@
 #include "Component.h"
+#include <iostream>
+
+using namespace std;
+
+Component::Component()
+{
+}
+
+void Component::Update()
+{
+	cout << "Update" << endl;
+}
+
+void Component::Draw()
+{
+	cout << "Draw" << endl;
+}
