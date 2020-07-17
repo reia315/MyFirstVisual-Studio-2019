adb6e98c91959708991d762121985342d85a818a
Saima <star15yuji@gmail.com>
2020-07-17 14:21:06 +0900

    update GameObjectManager.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
@@ -7,6 +7,6 @@ GameObjectManager::GameObjectManager()
 std::weak_ptr<GameObject> GameObjectManager::GeneratGameObject(const std::string& name)
 {
 	auto gameObject =std::make_shared<GameObject>(name);
-	m_gameObject.push_back(gameObject);
+	m_gameObject.push_front(gameObject);
 	return gameObject;
 }
