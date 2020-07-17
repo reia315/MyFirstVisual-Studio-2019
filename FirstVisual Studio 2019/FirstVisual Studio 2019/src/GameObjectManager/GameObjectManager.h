acdb7306436d104e8ef8e4096e77abe3abbb3a51
Saima <star15yuji@gmail.com>
2020-07-17 11:54:41 +0900

    update GameObjectManager.h

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.h
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.h
@@ -1,7 +1,16 @@
 #pragma once
-
+#include <list>
+#include <string>
+#include <vector>
+#include "../GameObject/GameObject.h"
 
 class GameObjectManager
 {
+public:
+	GameObjectManager();
 
+	GameObject GeneratGameObject(const std::string& name);
+
+private:
+	std::vector<GameObject> m_gameObject;
 };
