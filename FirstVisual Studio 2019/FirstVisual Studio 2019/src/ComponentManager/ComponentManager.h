73975d1b1570536d9caa8a03bc98709181eb9e41
Saima <star15yuji@gmail.com>
2020-07-17 15:50:26 +0900

    update ComponentManager.h

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.h
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <list>　　//前方配列
+#include <memory>　　　　　//スマートポインタ
+
+class Component;
+
+class ComponentManager
+{
+public:
+
+	//更新
+	static void update();
+	//描画
+	static void draw();
+	//コンポーネントクラスの生成
+	static std::weak_ptr<Component> GeneratComponent();
+
+
+private:
+
+	static std::list<std::shared_ptr<Component>>m_component;
+};
