deb00fc33f21a77849f7601e667a224f1504eb39
Saima <star15yuji@gmail.com>
2020-07-17 11:55:00 +0900

    update GameObjectManager.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
@@ -1,1 +1,12 @@
 #include "GameObjectManager.h"
+
+GameObjectManager::GameObjectManager()
+{
+}
+
+GameObject GameObjectManager::GeneratGameObject(const std::string& name)
+{
+	GameObject gameObject =GameObject(name);
+	m_gameObject.push_back(gameObject);
+	return gameObject;
+}
