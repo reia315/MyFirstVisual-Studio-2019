aa1606686ed3b450c2eac8834d272fc7a986bab0
Saima <star15yuji@gmail.com>
2020-07-17 15:50:57 +0900

    update ComponentManager.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.cpp
@@ -1,1 +1,25 @@
 #include "ComponentManager.h"
+#include "../Component/Component.h"
+
+void ComponentManager::update()
+{
+	for (const auto& component : m_component)
+	{
+		component->Update();
+	}
+}
+
+void ComponentManager::draw()
+{
+	for (const auto& component : m_component)
+	{
+		component->Draw();
+	}
+}
+
+std::weak_ptr<Component> ComponentManager::GeneratComponent()
+{
+	auto component = std::make_shared<Component>();
+	m_component.push_back(component);
+	return component;
+}
