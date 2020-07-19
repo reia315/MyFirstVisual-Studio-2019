bd852f33c757a59293242da2427f83bf643f7440
Saima <star15yuji@gmail.com>
2020-07-19 15:08:30 +0900

    update ComponentManager.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.cpp
@@ -1,6 +1,9 @@
 #include "ComponentManager.h"
 #include "../Component/Component.h"
 
+//静的変数のときは.cppで再定義する必要がある
+std::list<std::shared_ptr<Component>>ComponentManager::m_component;
+
 void ComponentManager::update()
 {
 	for (const auto& component : m_component)
