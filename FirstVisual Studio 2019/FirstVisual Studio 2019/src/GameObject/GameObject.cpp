914a9f65310fa97415d4224b960c0ae43e05405e
Saima <star15yuji@gmail.com>
2020-07-19 19:08:28 +0900

    update GameObject.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObject/GameObject.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObject/GameObject.cpp
@@ -1,10 +1,19 @@
 #include"GameObject.h"
+#include "../Component/Component.h"
+#include "../ComponentManager/ComponentManager.h"
 
 GameObject::GameObject(const std::string& name):m_name{name}{}
 
 const std::string& GameObject::GetName()const
 {
 	return m_name;
+}
+
+void GameObject::AddComponent()
+{
+	auto component = ComponentManager::GeneratComponent();
+	component.lock()->SetGameObject(weak_from_this());
+	m_components.push_back(component);
 }
 
 
