13da44df3c6bf6456536ee9b9e6c646bc327af50
Saima <star15yuji@gmail.com>
2020-07-21 15:49:08 +0900

    add Game.cpp

---
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/Game/Game.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "Game.h"
+#include "../GameObject/GameObject.h"
+#include "../ComponentManager/ComponentManager.h"
+#include "../GameObjectManager/GameObjectManager.h"
+#include <memory>
+
+using namespace std;
+
+void Game::Run()
+{
+	//コマンド入力値
+	string command = "";
+
+	{
+		auto component = ComponentManager::GenerateComponent();
+		GameObjectManager::GenerateGameObject("ObjectA").lock()->AddComponent(component);
+	}
+	{
+		auto component = ComponentManager::GenerateComponent();
+		GameObjectManager::GenerateGameObject("ObjectB").lock()->AddComponent(component);
+	}
+
+	cout << "プログラム開始" << endl;
+
+	//コマンド判別
+	while (command != "end")
+	{
+		cout << "フレーム更新" << endl;
+
+
+		ComponentManager::update();
+
+		cout << endl;
+
+		ComponentManager::draw();
+
+		cout << endl;
+
+		//updateとdrawの表示
+
+		cin >> command;
+
+
+		if (command == "add")
+		{
+			string name;
+			cout << "オブジェクト名入力 ->";
+			cin >> name;
+
+			{
+				auto component = ComponentManager::GenerateComponent();
+				GameObjectManager::GenerateGameObject(name).lock()->AddComponent(component);
+			}
+
+			ComponentManager::update();
+
+			std::cout << endl;
+
+			ComponentManager::draw();
+
+			std::cout << endl;
+		}
+
+		std::cout << endl;
+
+	}
+
+	//終了時に入力
+	std::cout << "プログラム終了" << endl;
+}
