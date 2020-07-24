544b717026932201ad739b2c1c12b8a40c432823
Saima <star15yuji@gmail.com>
2020-07-24 14:58:38 +0900

    update Component.h

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.h
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/ComponentManager/ComponentManager.h
@@ -13,10 +13,19 @@ public:
 	//描画
 	static void draw();
 	//コンポーネントクラスの生成
-    static std::weak_ptr<Component> GenerateComponent();
+	template<class T,class... Args >
+    static std::weak_ptr<Component> GenerateComponent(Args... args);
 
 
 private:
 
 	static std::list<std::shared_ptr<Component>>m_component;
 };
+
+template<class T,class ...Args>
+inline std::weak_ptr<Component> ComponentManager::GenerateComponent(Args ...args)
+{
+	auto component = std::make_shared<T>(args...);
+	m_component.push_back(component);
+	return component;
+}
