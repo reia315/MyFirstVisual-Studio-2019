47812c8a655303d7b6e711865152c475be2be593
Saima <star15yuji@gmail.com>
2020-07-28 10:48:43 +0900

    add DestoryObject.cpp

---
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/DestoryObject/DestoryObject.cpp
@@ -0,0 +1,16 @@
+#include "DestoryObject.h"
+#include "../GameObjectManager/GameObjectManager.h"
+#include "../GameObject/GameObject.h"
+#include "../ComponentManager/ComponentManager.h"
+
+DestoryObject::DestoryObject(const std::string& name) :m_name{ name } {}
+
+void DestoryObject::Update()
+{
+	auto object =  GameObjectManager::SearchObject(m_name);
+
+	for (const auto& gameObject : object)
+	{
+		gameObject.lock()->Destroy();
+	}
+}
