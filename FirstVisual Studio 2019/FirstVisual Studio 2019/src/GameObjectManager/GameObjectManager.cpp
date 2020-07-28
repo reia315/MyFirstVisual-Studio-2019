3716ce468c098c1217efe5fe42534453d6dd5784
Saima <star15yuji@gmail.com>
2020-07-28 10:50:46 +0900

    update GameObjectManager.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/GameObjectManager/GameObjectManager.cpp
@@ -1,11 +1,39 @@
 #include "GameObjectManager.h"
 #include "../GameObject/GameObject.h"
+#include "../DestoryObject/DestoryObject.h"
 
 std::forward_list<std::shared_ptr<GameObject>>GameObjectManager::m_gameObject;
+
+
+
+std::forward_list<std::weak_ptr<GameObject>> GameObjectManager::SearchObject(const std::string& name)
+{
+	std::forward_list<std::weak_ptr<GameObject>> match;
+
+	for (const auto& gameObject : m_gameObject)
+	{
+		if (gameObject->GetName() == name)
+		{
+			match.push_front(gameObject);
+		}
+	}
+
+	return match;
+}
 
 std::weak_ptr<GameObject> GameObjectManager::GenerateGameObject(const std::string& name)
 {
 	auto gameObject =std::make_shared<GameObject>(name);
 	m_gameObject.push_front(gameObject);
 	return gameObject;
+}
+
+void GameObjectManager::DeleteGameObject(const std::string& name)
+{	
+	//const auto& gameObject = std::remove(m_gameObject.begin(), m_gameObject.end(), name);
+
+
+	//ラムダ式[]ランダムチャプター（）パラメーター定義節｛｝複合ステートメント（）関数呼び出し式；
+	//gameObjectの中でIsDeadがtrueになってる要素を削除する
+	m_gameObject.remove_if([](const auto& gameObject) {return gameObject->IsDead(); });//ラムダ式を使用
 }
