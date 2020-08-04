2f236b1ef2273251e5ccb0a84e510ddf0cc9a2cd
Saima <star15yuji@gmail.com>
2020-08-04 15:05:09 +0900

    update Comoponent.h

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/Component/Component.h
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/Component/Component.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <string>
 
 //前方宣言
 class GameObject;
@@ -14,18 +15,16 @@ public:
 	virtual void Update() {};
 	//描画処理
 	virtual void Draw() {};
-
 	virtual std::weak_ptr<Component>CloneComponent() const = 0;
+	virtual void Input(const std::string& inputStr) {};
+
 
 	//このコンポーネントのユーザーを取得する
 	const std::weak_ptr<GameObject>& GetGameObject() const; 
-
 	//このコンポーネントのユーザーを設定する
 	void SetGameObject(const std::weak_ptr<GameObject>& gameObject) ;
-
 	//死亡フラグを立てる
 	void Destroy();
-
 	//死んでいるかの状態を返す
 	bool IsDead() const;
 
