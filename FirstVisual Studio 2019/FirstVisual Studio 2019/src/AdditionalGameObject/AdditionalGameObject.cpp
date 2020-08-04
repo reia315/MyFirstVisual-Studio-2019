bdf2b53d7c02926b62075c1be963807b9d145f54
Saima <star15yuji@gmail.com>
2020-08-04 15:23:56 +0900

    add AdditioonalGameObject.cpp

---
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/AdditionalGameObject/AdditionalGameObject.cpp
@@ -0,0 +1,20 @@
+#include "AdditionalGameObject.h"
+#include "../ComponentManager/ComponentManager.h"
+#include "../GameObjectManager/GameObjectManager.h"
+#include "../TestOutPut/TestOutPut.h"
+#include "../GameObject/GameObject.h"
+#include <iostream>
+
+
+AdditionalGameObject::AdditionalGameObject(const std::weak_ptr<const AdditionalGameObject>& other){}
+
+std::weak_ptr<Component> AdditionalGameObject::CloneComponent() const
+{
+	return ComponentManager::GenerateComponent<AdditionalGameObject>(std::dynamic_pointer_cast<const AdditionalGameObject>(shared_from_this()));
+}
+
+void AdditionalGameObject::Input(const std::string& inputStr)
+{
+	auto gameObject = GameObjectManager::GenerateGameObject(inputStr);
+	gameObject.lock()->CreateComponent<TestOutPut>();
+}
