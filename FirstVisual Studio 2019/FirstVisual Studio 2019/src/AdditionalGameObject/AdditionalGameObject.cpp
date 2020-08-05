3e24ac578aa83ead23e03b297d53aa36f9bf8df3
Saima <star15yuji@gmail.com>
2020-08-05 13:50:36 +0900

    update AddtionalgameObject.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/AdditionalGameObject/AdditionalGameObject.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/AdditionalGameObject/AdditionalGameObject.cpp
@@ -5,9 +5,13 @@
 #include "../GameObject/GameObject.h"
 
 
-AdditionalGameObject::AdditionalGameObject(char named, const std::string& findCommand) :m_gameObjectName{ named }, m_findCommand{findCommand} {}
+AdditionalGameObject::AdditionalGameObject(const std::string& inputStr,char separated ) :
+	m_createName{inputStr},
+	m_separated { separated }{}
 
-AdditionalGameObject::AdditionalGameObject(const std::weak_ptr<const AdditionalGameObject>& other) :m_gameObjectName{ other.lock()->m_gameObjectName }, m_findCommand{other.lock()->m_findCommand} {}
+AdditionalGameObject::AdditionalGameObject(const std::weak_ptr<const AdditionalGameObject>& other) :
+	m_createName{other.lock()->m_createName},
+	m_separated{ other.lock()->m_separated } {}
 
 std::weak_ptr<Component> AdditionalGameObject::CloneComponent() const
 {
@@ -18,10 +22,10 @@ std::weak_ptr<Component> AdditionalGameObject::CloneComponent() const
 //コマンドを２つで区切る前方でadd後方で名前を受け取る
 void AdditionalGameObject::Input(const std::string& inputStr)
 {
-	int idx = inputStr.find(m_gameObjectName);
+	int idx = inputStr.find(m_separated);
 
 	std::string command = inputStr.substr(0, idx);
-	if (command == m_findCommand)
+	if (command == m_createName)
 	{
 		std::string objectName = inputStr.substr(idx + 1, inputStr.length());
 
