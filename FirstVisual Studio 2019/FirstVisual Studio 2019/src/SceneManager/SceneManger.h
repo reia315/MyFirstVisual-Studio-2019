035b9de802ddeeb9c3362469ce4f88e6c20067b0
Saima <star15yuji@gmail.com>
2020-08-07 14:38:31 +0900

    add SceneManger.h

---
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/SceneManager/SceneManger.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <memory>
+#include "../Scene/Scene.h"
+
+class SceneManger
+{
+public:
+
+	template<class T,class... Args>
+	static void SceneFlow(const Args&... args);
+private:
+	static std::shared_ptr<Scene>m_rightNowScene;
+};
+
+template<class T, class ...Args>
+inline void SceneManger::SceneFlow(const Args& ...args)
+{
+	if (m_rightNowScene != nullptr)
+	{
+		//シーン終了処理
+		m_rightNowScene->End();
+	}
+
+	//新しいシーンのセット
+	//reset:リソースの所有権を放棄し、新たなリソースの所有権を設定する
+	m_rightNowScene.reset(new T(args...));
+
+	//シーン開始処理
+	m_rightNowScene->State();
+}
