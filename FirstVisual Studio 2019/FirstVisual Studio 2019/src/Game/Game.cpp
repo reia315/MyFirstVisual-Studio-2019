4e01eb2de5dcd0403227e5013baffdbd0f61f393
Saima <star15yuji@gmail.com>
2020-08-07 11:26:23 +0900

    update Game.cpp

--- FirstVisual Studio 2019/FirstVisual Studio 2019/src/Game/Game.cpp
+++ FirstVisual Studio 2019/FirstVisual Studio 2019/src/Game/Game.cpp
@@ -7,8 +7,11 @@
 #include "../DestoryObject/DestoryObject.h"
 #include "../CloneObject/CloneObject.h"
 #include "../AdditionalGameObject/AdditionalGameObject.h"
+#include "../EndProcessing/EndProcessing.h"
 
 using namespace std;
+
+bool Game::isEnd{ false };
 
 void Game::Run()
 {
@@ -20,10 +23,11 @@ void Game::Run()
 	gameObject.lock()->CreateComponent<DestoryObject>("ObjectB");
 	gameObject.lock()->CreateComponent<CloneObject>(3);
 	gameObject.lock()->CreateComponent<AdditionalGameObject>("add", ',');
+	gameObject.lock()->CreateComponent<EndProcessing>("end");
 	cout << "プログラム開始" << endl;
 
 	//コマンド判別
-	while (command != "end")
+	while (!isEnd)
 	{
 
 		cout << "フレーム更新" << endl;
@@ -53,4 +57,9 @@ void Game::Run()
 	}
 	//終了時に入力
 	std::cout << "プログラム終了" << endl;
+}
+
+void Game::End()
+{
+	isEnd = true;
 }
